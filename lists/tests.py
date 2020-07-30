2a78f5e1dc233405685d370d46a75567053f0984
wang-tf <ternencewang2015@outlook.com>
2020-07-30 18:39:28 +0800

    test http

--- lists/tests.py
+++ lists/tests.py
@@ -8,3 +8,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endwith('</html>'))
